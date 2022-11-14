7e9e857610a2635112d075f76ae548119f53c587
Lilith Hafner <Lilith.Hafner@gmail.com>
2022-11-14 22:11:09 +0600

    upgrade coding

--- src/ManualImageCoding.jl
+++ src/ManualImageCoding.jl
@@ -8,79 +8,56 @@ module ManualImageCoding
 # Multiple entries per image
 # Automatic column names
 
-testimages = ["airplaneF16", "autumn_leaves", "barbara_color", "barbara_gray_512", "bark_512", "bark_he_512", "beach_sand_512", "beach_sand_he_512", "blobs", "brick_wall_512", "brick_wall_he_512", "calf_leather_512", "calf_leather_he_512", "cameraman", "chelsea", "coffee", "earth_apollo17", "fabio_color_256", "fabio_color_512", "fabio_gray_256", "fabio_gray_512", "grass_512", "grass_he_512", "hela-cells", "herringbone_weave_512", "herringbone_weave_he_512", "house", "jetplane", "lake_color", "lake_gray", "lena_color_256", "lena_color_512", "lena_gray_16bit", "lena_gray_256", "lena_gray_512", "lighthouse", "livingroom", "m51", "mandril_color", "mandril_gray", "mandrill", "monarch_color", "monarch_color_256", "moonsurface", "morphology_test_512", "mountainstream", "mri-stack", "multi-channel-time-series.ome", "peppers_color", "peppers_gray", "pigskin_512", "pigskin_he_512", "pirate", "plastic_bubbles_512", "plastic_bubbles_he_512", "raffia_512", "raffia_he_512", "resolution_test_1920", "resolution_test_512", "simple_3d_ball", "simple_3d_psf", "straw_512", "straw_he_512", "sudoku", "toucan", "walkbridge", "water_512", "water_he_512", "woman_blonde", "woman_darkhair", "wood_grain_512", "wood_grain_he_512", "woolen_cloth_512", "woolen_cloth_he_512"]
-image_extensions = [".png", ".jpg", ".jpeg"]
-imgpath = "../../../Desktop/img.png"
+using Gtk, Random
 
-using Gtk, TestImages
+export code
 
-export imshow, imshowall, page, imgpath, code
+const IMAGE_EXTENSIONS = [".png", ".jpg", ".jpeg"]
+const FILE_NAME = "codings.csv"
+const DELIM = ", "
+is_image_path(path) = any(endswith(path, ext) for ext in IMAGE_EXTENSIONS)
 
-function imshow(filename::AbstractString; title="Image")
-    isfile(filename) || error("File not found: $filename")
-    i = Gtk.GtkImage(filename)
-    w = GtkWindow(i, "Image")
-    show(i)
-    nothing
-end
-
-function imshowall(path::AbstractString; limit=10, kw...)
+function search(path::AbstractString; recursive::Bool=true, filter::Function=is_image_path)
+    out = String[]
     for (root, _, files) in walkdir(path)
         for file in files
             filename = joinpath(root, file)
-            if any(endswith(filename, ext) for ext in image_extensions)
-                imshow(filename; kw...)
-                limit -= 1
-                limit <= 0 && return
-            end
+            filter(filename) && push!(out, filename)
         end
+        !recursive && break
     end
+    out
 end
 
-@guarded function page(path::AbstractString; title="Images")
-    w = GtkWindow(title)
-    c = Channel{Bool}()
-
-    @guarded signal_connect(w, "key-press-event") do _, event
-        exit = event.keyval ∈ (113, 119)
-        exit && Gtk.destroy(w) # q, w
-        put!(c, exit)
-        nothing
+function get_uncoded(path::AbstractString; shuffle=true, verbose=true, kw...)
+    out = search(path; kw...)
+    total = length(out)
+    for f in out
+        occursin(DELIM, joinpath(path, f)) && println("WARNING: Delimiter \"$DELIM\" found in file \"$f\". Consider renaming the file.")
     end
-
-    @guarded signal_connect(w, "delete-event") do _, event
-        put!(c, true)
-        nothing
-    end
-
-    i = nothing
-    for (root, _, files) in walkdir(path)
-        for file in files
-            filename = joinpath(root, file)
-            if any(endswith(filename, ext) for ext in image_extensions)
-                sleep(.001) # Hand control back to Gtk
-                i === nothing || destroy(i)
-                i = Gtk.GtkImage(filename)
-                push!(w, i)
-                show(i)
-                take!(c) && return
-            end
+    filename = joinpath(path, FILE_NAME)
+    if isfile(filename)
+        coded = open(filename, "r") do f
+            Set(first(split(line, DELIM)) for line in readlines(f))
         end
+        filter!(x -> !(relpath(x, path) in coded), out)
     end
-
+    shuffle && shuffle!(out)
+    verbose && println("$(total-length(out))/$total ($(round(Int, 100*(total-length(out))/total))%) images coded. $(length(out)) left.")
+    out
 end
 
-include("Keyboard.jl")
 function code(path::AbstractString; title="Coding")
     w = GtkWindow(title)
     c = Channel{Bool}()
-    #k = Keyboard(w, exit_on_command_w=false, exit_on_command_q=false)
 
     keys = UInt8[]
 
     @guarded signal_connect(w, "key-press-event") do _, event
         # The key is enter    && Any non-shift modifier is pressed
         event.keyval == 65293 && #=event.state & 0x11111101 != 0 &&=# put!(c, false)
+        # The key is escape
+        event.keyval == 65307 && (Gtk.destroy(w); put!(c, true))
         # The key is ascii
         event.keyval <= 128 && push!(keys, event.keyval)
         nothing
@@ -91,28 +68,26 @@ function code(path::AbstractString; title="Coding")
         nothing
     end
 
-
-    open(joinpath(path, "codings.txt"), "a") do io
+    files = get_uncoded(path)
+    open(joinpath(path, FILE_NAME), "a") do io
         i = nothing
-        for (root, _, files) in walkdir(path)
-            for file in files
-                filename = joinpath(root, file)
-                if any(endswith(filename, ext) for ext in image_extensions)
+        for file in files # For each image file
+            sleep(.001) # Hand control back to Gtk
+            i === nothing || destroy(i)
+            i = Gtk.GtkImage(file)
+            push!(w, i)
 
-                    # For each image file
+            show(i)
+            resize!(w, 500, 500)
+            sleep(.001)
 
-                    sleep(.001) # Hand control back to Gtk
-                    i === nothing || destroy(i)
-                    i = Gtk.GtkImage(filename)
-                    push!(w, i)
-                    show(i)
-                    exit = take!(c)
-                    write(io, relpath(filename, path), ',', String(keys), '\n')
-                    if exit
-                        return
-                    end
-                end
-            end
+            take!(c) && break # If someone makes a typo, they can exit. Save on explicit enter only.
+
+            str = relpath(file, path) * DELIM * String(keys) * '\n'
+            write(io, str)
+            flush(io)
+            print(str)
+
         end
     end
 end
